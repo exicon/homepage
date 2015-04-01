5a512925725e2997a5f0763cff9810afabe025fe
Laura Sempere <laura@exiconglobal.com>
2015-04-01 10:45:03 +0800

    [#90662098] App Builder public page video and links added

--- src/clj/config.clj
+++ src/clj/config.clj
@@ -2,3 +2,4 @@
 
 (defmacro getenv [key] (System/getenv key))
 (defmacro appboard-url [] (or (getenv "APPBOARD_URL") "http://localhost:3000"))
+(defmacro appbuilder-url [] (or (getenv "APPBUILDER_URL") "http://localhost:3004"))
