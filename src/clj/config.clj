c8ac98c0b25261d4ad57c105019088a85551354d
Laura Sempere & Ayaz Maroof <ayaz@exiconglobal.com>
2015-06-03 18:18:52 +0800

    [#95593146] Change timeline-url to timeline-src

--- src/clj/config.clj
+++ src/clj/config.clj
@@ -3,4 +3,4 @@
 (defmacro getenv [key] (System/getenv key))
 (defmacro appboard-url [] (or (getenv "APPBOARD_URL") "http://localhost:3000"))
 (defmacro appbuilder-url [] (or (getenv "APPBUILDER_URL") "http://localhost:3004"))
-(defmacro timeline-url [] (or (getenv "TIMELINE_URL") "//cdn.knightlab.com/libs/timeline/latest/embed/index.html?source=1_OogiCWCFjzW1iSvRpifWbdWkERFyEAhQKWwNjVw-lQ&font=Bevan-PotanoSans&maptype=toner&lang=en&height=650"))
+(defmacro timeline-src [] (or (getenv "TIMELINE_SRC") "//cdn.knightlab.com/libs/timeline/latest/embed/index.html?source=1_OogiCWCFjzW1iSvRpifWbdWkERFyEAhQKWwNjVw-lQ&font=Bevan-PotanoSans&maptype=toner&lang=en&height=650"))
