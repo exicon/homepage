b52b99fcfc6e79ff9ae92b65c3337e070a78cb2a
Peter Romfeld & Tamas Herman <peter+tamas@exiconglobal.com>
2015-08-12 17:52:33 +0800

    Hoplon6ificcation

--- src/clj/homepage/api.clj
+++
@@ -1,6 +0,0 @@
-(ns homepage.api
-	(:require
-		[tailrecursion.castra :refer [defrpc]]))
-
-(defrpc get-state []
-	{:random (rand-int 100)})
---
+++ src/config.clj
@@ -0,0 +1,13 @@
+(ns config)
+
+(defmacro getenv [key] (System/getenv key))
+(defmacro appboard-url [] (or (getenv "APPBOARD_URL") "http://localhost:3000"))
+(defmacro appbuilder-url [] (or (getenv "APPBUILDER_URL") "http://localhost:3004"))
+(defmacro timeline-src []
+  (or (getenv "TIMELINE_SRC")
+      "//cdn.knightlab.com/libs/timeline/latest/embed/index.html?source=1_OogiCWCFjzW1iSvRpifWbdWkERFyEAhQKWwNjVw-lQ&font=Bevan-PotanoSans&maptype=toner&lang=en&height=650"))
+(defmacro inspectlet-feature []
+  (= "true" (clojure.string/lower-case (str (getenv "INSPECTLET_FEATURE")))))
+(defmacro inspectlet-wid [] (getenv "INSPECTLET_WID"))
+(defmacro hs-portal-id [] (getenv "HS_PORTAL_ID"))
+(defmacro hs-newsletter-id [] (getenv "HS_NEWSLETTER_ID"))
