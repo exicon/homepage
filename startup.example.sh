e08018f4911df2e9d53be2e86bd229813ce5dc1a
Laura & Stewart <laura@exiconglobal.com>
2015-08-13 16:34:45 +0800

    example startup.sh (temporary till boot-tasks override)

---
+++ startup.example.sh
@@ -0,0 +1,6 @@
+export HS_CONTACT_US_ID=XXXXXX (https://app.hubspot.com/forms/511335/75a9222d-e214-4a89-a778-97de1f9c5ef9/edit/)
+export HS_PORTAL_ID=XXXXXX
+export HS_NEWSLETTER_ID=XXXXXXX
+export HS_APP_CALC_ID=XXXXXXX
+boot1 development
+
