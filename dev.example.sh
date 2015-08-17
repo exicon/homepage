683b7745b5b7d991dbb6227b445ad90b4d4463e7
Tamas Herman & Laura Sempere <tamas+laura@exiconglobal.com>
2015-08-17 18:01:21 +0800

    [#101093360] Dev HubSpot form IDs

--- dev.example.sh
+++ dev.example.sh
@@ -1,7 +1,7 @@
-export HS_CONTACT_US_ID=XXXXXX # https://app.hubspot.com/forms/511335/75a9222d-e214-4a89-a778-97de1f9c5ef9/edit/
-export HS_PORTAL_ID=XXXXXX
-export HS_NEWSLETTER_ID=XXXXXXX
-export HS_APP_CALC_ID=XXXXXXX
+export HS_PORTAL_ID=511335
+export HS_CONTACT_US_ID=75a9222d-e214-4a89-a778-97de1f9c5ef9 # https://app.hubspot.com/forms/511335/75a9222d-e214-4a89-a778-97de1f9c5ef9/edit/
+export HS_NEWSLETTER_ID=93a2e67a-66d7-499c-8122-04b3f9b8d466 # https://app.hubspot.com/forms/511335/93a2e67a-66d7-499c-8122-04b3f9b8d466/edit/
+export HS_APP_CALC_ID=0d6c99ac-c675-4e45-b6b1-8d623effef48 # https://app.hubspot.com/forms/511335/0d6c99ac-c675-4e45-b6b1-8d623effef48/edit/
 export INSPECTLET_FEATURE=true
 export INSPECTLET_WID=1724490325
 boot dev
