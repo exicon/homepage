53a7efc4719447b8f781c739d5f694e1d90bd587
Peter Romfeld & Laura Sempere <laura+peter@exiconglobal.com>
2015-12-11 13:12:34 +0800

    [#109870242] Tell Node-Appboard to Redirect new signups to AppBoard

--- src/config.clj
+++ src/config.clj
@@ -4,7 +4,7 @@
 
 (defmacro getenv [key] (System/getenv key))
 (defmacro appboard-url [] (or (getenv "APPBOARD_URL") "http://localhost:3000"))
-(defmacro appbuilder-url [] (or (getenv "APPBUILDER_URL") "http://localhost:3004"))
+(defmacro appbuilder-url [] (or (getenv "APPBUILDER_URL") "http://localhost:3104"))
 (defmacro timeline-src []
   (or (getenv "TIMELINE_SRC")
       "//cdn.knightlab.com/libs/timeline/latest/embed/index.html?source=1_OogiCWCFjzW1iSvRpifWbdWkERFyEAhQKWwNjVw-lQ&font=Bevan-PotanoSans&maptype=toner&lang=en&height=650"))
